00abb551ddaf2b4db77f2dd579a0e391e0f11307
tdoan <tdoan5@fordham.edu>
2018-03-29 02:03:09 -0400

    Commit Hello World flask app

---
+++ model/hello.py
@@ -0,0 +1,27 @@
+from flask import Flask
+# unique name for Flask class instance
+app = Flask(__name__)
+
+# decorator
+@app.route('/')
+def index():
+    return "Index Page"
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # show the user profile for that user
+    return "User %s" % username 
+
+@app.route('/hello')
+def hello_world():
+    return "Hello World!"
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    return "Post %d" % post_id
+
+# create a server to wait for URL to enter, then follow 
+# decorator route to run functions hello world
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
