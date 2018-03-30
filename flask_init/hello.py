3436af8d856304fd332f8ed96937e8824b53cb9e
tdoan <tdoan5@fordham.edu>
2018-03-30 12:36:12 -0400

    add templates

--- flask_init/hello.py
+++ flask_init/hello.py
@@ -1,11 +1,17 @@
-from flask import Flask, url_for
+from flask import Flask, url_for, render_template
 # unique name for Flask class instance
 app = Flask(__name__)
 
+"""
 # decorator
 @app.route('/login', methods = ['GET'])
 def login():
     return '<form methods="get" action="/login"><input type="text" name="username" /><p><button type="submit">Submit</button></form>'
+"""
+@app.route('/hello')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name_template = name)
 
 # create a server to wait for URL to enter, then follow 
 # decorator routes to run functions
--- flask_init/login.py
+++ flask_init/login.py
@@ -1,16 +1,15 @@
 import os
 
-from flask import Flask, request
+from flask import Flask, request, render_template
 # unique name for Flask class instance
 app = Flask(__name__)
 
 # decorator
-@app.route('/login', methods = ["GET", "POST"])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    if request.method == "POST":
-        return 'username is ' + request.values["username"]
-    else:
-        return '<form methods="POST" action="/login"><input type="text" name="username" /><p><button type="submit">Submit</button></form>'
+    if request.method == 'POST':
+        return "User %s logged in" % request.form['username']
+    return render_template('login.html')
 
 # create a server to wait for URL to enter, then follow 
 # decorator routes to run functions
